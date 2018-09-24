067c2cf166b34e2aed99345c910f7256cde0ce9d
FlatEarthTruther <ret0@goretoy.com>
2018-09-24 16:51:18 -0500

    404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,8 @@ import React from 'react'
 const NotFoundPage = () => (
   <div>
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>Page not found.</p>
+    <img className="hidden" src="https://a.alexgoretoy.com/track?idsite=alexgoretoy.com&amp;rec=1" alt="" />
   </div>
 )
 
