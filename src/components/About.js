320c626380961a5aa29f2ca6f05d4aab8f3663e7
FlatEarthTruther <ret0@goretoy.com>
2018-09-27 01:20:57 -0500

    update about with audiobible soundcloud filter

--- src/components/About.js
+++ src/components/About.js
@@ -5,25 +5,6 @@ import { FaCloudDownload, FaSkype } from "react-icons/lib/fa";
 import profilePic1 from "../assets/images/profilepic1.jpg";
 import profilePic2 from "../assets/images/profilepic2.jpg";
 
-// <div className="columns contact-details animated pulse">
-//             <h2>Contact Alex</h2>
-//             <div className="row">
-//               <div className="columns">
-//                 <span>Aleksandr Goretoy</span>
-//                 <br />
-//                 <span><OutboundLink href="tel:+13107410216">+1 (310) 741-0216</OutboundLink></span>
-//                 <br />
-//                 <span><OutboundLink href="mailto:alex@goretoy.com">alex@goretoy.com</OutboundLink></span>
-//               </div>
-//               <div className="columns">
-//                 <span>Remote Work Only</span>
-//                 <br />
-//                 <span><OutboundLink href="//goretoy.com">Hire me @ $120/hr</OutboundLink></span>
-//                 <br />
-//                 <span><FaSkype />&nbsp;&nbsp;&nbsp;&nbsp;<OutboundLink href="skype:superleetdude">superleetdude</OutboundLink></span>
-//               </div>
-//             </div>
-//           </div>
 const About = () => (
   <section id="about">
     <div className="row">
@@ -35,7 +16,7 @@ const About = () => (
       <div className="nine columns main-col">
         <h2 className="animated slideInDown">About Me</h2>
         <p>
-          I've been working with technology since my teenage years when I bought my first computer at 15 with my paperboy money. I started with VS.net development in 2003, then grew tired of MS, learned Linux, open-source web development and have been doing it very successfully for many years now. I prefer to work remotely not on-site, since it's all internet based industry, it makes things easier on me. I'm very detail oriented and like to make things that are solid and scale beyond expectations. I love open-source, hot-pockets and kittens. <OutboundLink href="http://waterizlife.com">Ask me about the water that I drink and get you some!</OutboundLink> I'm self-taught and I'm not cheap! I don't need a degree to make secure, scalable applications. I have real world production experience and that goes a long way compared to someone who only knows how to do what they're told and how to think. There's a good reason why I dropped out of school of indoctrination camps and I never looked back. I'm not some circus clown! When I am not working or learning some new technology, or creating something from nothing, I like to seek the truth and do research about how the Earth is Flat, amongst other topics! I also like to read <OutboundLink href="https://top-secret-documents.com">top secret documents</OutboundLink> that have been declassified, and watch educational videos for fun. I highly recommend you read the <OutboundLink href="https://biblehub.com/kjv/" title="KJV is the only true word of God, the others are esoteric satanic versions, just FYI!">KJV Bible</OutboundLink>, if you know whats good for you! <OutboundLink href="https://audiobible.life" title="Proceed with the github SSL to view the site">Learn more</OutboundLink>
+          I've been working with technology since my teenage years when I bought my first computer at 15 with my paperboy money. I started with VS.net development in 2003, then grew tired of MS, learned Linux, open-source web development and have been doing it very successfully for many years now. I prefer to work remotely not on-site, since it's all internet based industry, it makes things easier on me. I'm very detail oriented and like to make things that are solid and scale beyond expectations. I love open-source, hot-pockets and kittens. <OutboundLink href="http://waterizlife.com">Ask me about the water that I drink and get you some!</OutboundLink> I'm self-taught and I'm not cheap! I don't need a degree to make secure, scalable applications. I have real world production experience and that goes a long way compared to someone who only knows how to do what they're told and how to think. There's a good reason why I dropped out of school of indoctrination camps and I never looked back. I'm not some circus clown! When I am not working or learning some new technology, or creating something from nothing, I like to seek the truth and do research about how the Earth is Flat, amongst other topics! I also like to read <OutboundLink href="https://top-secret-documents.com">top secret documents</OutboundLink> that have been declassified, and watch educational videos for fun. I highly recommend you read the <OutboundLink href="https://biblehub.com/kjv/" title="KJV is the only true word of God, the others are esoteric satanic versions, just FYI!">KJV Bible</OutboundLink>, or listen to the <OutboundLink href="https://soundcloud.com/search/sets?q=audiobible&filter.genre_or_tag=audiobooks">AudioBible</OutboundLink>.
         </p>
         <div className="row">
 
